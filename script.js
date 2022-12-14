cca538b5d45ed90789225a47eaf04fecf522f70a
mint5150 <musia.home@gmail.com>
2022-12-15 08:48:37 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var variableNumber = 1234;
+console.log(variableNumber);
+
+var variableText = "Text";
+console.log(variableText);
+
+var variableBoolean = true;
+console.log(variableBoolean);
+
+var addNumber = 10 + 20;
+console.log(addNumber);
+
+var subtractNumber = 20 - 10;
+console.log(subtractNumber);
+
+var multiplyNumber = 50 * 2;
+console.log(multiplyNumber);
+
+var dividedNumber = 20 / 2;
+console.log(dividedNumber);
+
+var combinationText = "12" + "34";
+console.log(combinationText);
+
+
